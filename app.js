4ff67698cd69babcba0a89b2ee8c3b0ec8e5cd1f
Eileen Li <eileenlimur@gmail.com>
2020-09-16 23:20:29 -0400

    add res.send. add req.body params to replace hard-coded

--- app.js
+++ app.js
@@ -25,12 +25,13 @@ app.post('/api/translate', function(req, res) {
     serviceUrl: process.env.TRANSLATOR_ENDPOINT
   });
   const translateParams = {
-    text: 'Hello, how are you today?',
-    target: 'es'
+    text: req.body.text,
+    target: req.body.target_language
   };
   languageTranslator.translate(translateParams)
   .then(translationResult => {
     console.log(JSON.stringify(translationResult, null, 2));
+    res.send(JSON.stringify(translationResult, null, 2));
   })
   .catch(err=> {
     console.log('error:', err);
